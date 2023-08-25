3e19a88bed5ee02ab90a7e4daaf02404024d94b7
Marina Paskal <paskalm@rogers.com>
2023-08-25 13:20:25 -0400

    Add content to home page

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function navBtnClick() {
-  let navStatus = document.getElementById("topNavbar");
-  console.log("navStatus ", navStatus);
-  if (navStatus.className === "navbar") {
-    navStatus.className += " nav-responsive";
-  } else {
-    navStatus.className = "navbar";
-  }
-}
+// let toggleBtn = document.querySelector(".toggle-btn");
+// let toggleBtnIcon = document.querySelector(".toggle-btn i");
+// let dropDownMenu = document.querySelector(".navbar-links-bars");
+
+// console.log("toggleBtn ", toggleBtn);
+// console.log("toggleBtnIcon", toggleBtnIcon);
+// console.log("dropDownMenu", dropDownMenu);
+
+// function navBtnClick() {
+//  // dropDownMenu.classList.replace("navbar-links","navbar-links-bars");
+//   dropDownMenu.classList.toggle('open');
+//   console.log("dropDownMenu1", dropDownMenu);
+//   const isOpen = dropDownMenu.classList.contains('open');
+//   console.log("isOpen ", isOpen);
+//   //isOpen ? dropDownMenu.classList.replace("navbar-links","navbar-links-bars") : dropDownMenu.classList.replace("navbar-links-bars","navbar-links");
+//   isOpen ? dropDownMenu.style.visibility = "visible" : dropDownMenu.style.visibility = "hidden";
+// }
+
+const hamburger = document.querySelector('.toggle-btn');
+const navLink = document.querySelector('.navbar-links');
+
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
 
 // copyright
 function todayDate() {
